8f2b378b92b9ed0c7f8f6ddd08369164f39478b9
pedromdcr <pedromdcrodrigues@gmail.com>
2017-08-31 23:26:11 +0100

    second commit

---
+++ TextFileConcatenator/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TextFileConcatenator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose files to combine:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Choose new .txt file name:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 95);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Combine!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(213, 205);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "About...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(213, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 140);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(365, 253);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text File Merger";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TextFileConcatenator/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextFileConcatenator
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        string path = @"C:\Users\Pedro\unityWorkspace\ExpertInterface\ExpertInterface\Logs";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool IsValidFilename(string testName)
+        {
+            Regex containsABadCharacter = new Regex("[" + Regex.Escape(new string(Path.GetInvalidPathChars())) + "]");
+
+            if (containsABadCharacter.IsMatch(testName)) { return false; };
+
+            // other checks for UNC, drive-path format, etc
+
+            return true;
+        }
+
+        private void mergeFiles(string newFilename)
+        {
+            if(ofd.FileNames.Length > 0)
+            {
+                string[] fileNames = ofd.FileNames;
+                
+                if(IsValidFilename(newFilename) && newFilename.Length > 0)
+                {
+                    if (!newFilename.EndsWith(".txt"))
+                        newFilename += ".txt";
+                    const int chunkSize = 2 * 1024; // 2KB
+                    using (var output = File.Create(path + "\\" + newFilename))
+                    {
+                        for(int i = 0; i < fileNames.Length; i++)
+                        {
+                            if(i != fileNames.Length - 1)
+                                File.AppendAllText(fileNames[i], "\n");
+                            using (var input = File.OpenRead(fileNames[i]))
+                            {
+                                var buffer = new byte[chunkSize];
+                                int bytesRead;
+                                while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    output.Write(buffer, 0, bytesRead);
+                                }
+                            }
+                            
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+            ofd.Multiselect = true; //sets to multiple selects
+            if (Directory.Exists(path))
+                ofd.InitialDirectory = path;
+            ofd.Filter = "Text files (*.txt)|*.txt";
+            ofd.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(ofd.FileNames.Length == 0)
+                MessageBox.Show(this, "Please select which files to combine before proceding.", "No files selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (textBox1.Text.Length == 0)
+                MessageBox.Show(this, "A name for the new file is required. Try again.", "Missing file name", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else mergeFiles(textBox1.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Icon made by Freepik from www.flaticon.com.", "Credit info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
